f7dbfa0816b1f35b6b363413dd925003535b517e
pozdny <pozdny@mail.ru>
2016-07-18 04:06:27 +0600

    final

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -93,7 +93,7 @@ gulp.task('css', () => {
 
 gulp.task('sass', () => {
     return gulp.src(config.pathDevSCSS + '/**/*.scss')
-        .pipe(sass(globalConfig.baseDir + '/scss', {sourcemap: true}))
+        .pipe(sass())
         .pipe(gulp.dest(config.pathDevCSS))
         .pipe(browserSync.reload({
             stream: true
